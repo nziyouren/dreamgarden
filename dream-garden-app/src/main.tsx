cef9d1cdf1544bba9f94cfa50f499536a9a310d5
nziyouren <nziyouren@gmail.com>
2026-02-06 20:02:25 +0800

    *auto connect wallet set

--- dream-garden-app/src/main.tsx
+++ dream-garden-app/src/main.tsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <SuiClientProvider networks={networkConfig} defaultNetwork="mainnet">
-        <WalletProvider>
+        <WalletProvider autoConnect>
           <App />
         </WalletProvider>
       </SuiClientProvider>
